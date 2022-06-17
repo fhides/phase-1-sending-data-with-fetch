d8d55c59da891b7c8f4dc12a8a5d8ae79a5338eb
fhidesjoseph <fhidesjoseph@gmail.com>
2022-06-17 13:59:55 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+let name = document.querySelector('#Name').value;
+let email = document.querySelector('#email').value;
+
+const submitData=(name,email)=>{
+    const formData={
+        name: name,
+        email: email,
+    };
+    const configurationObject = {
+        method: "POST",
+        headers: {"Content-Type": "application/json",
+        Accept: "application/json",
+    },
+    body: JSON.stringify(formData),
+    };
+return fetch("http://localhost:3000/users", configurationObject)
+    .then(responce => responce.json())
+    .then(data => {
+        document.body.innerHTML = data.id
+    })
+    .catch(err => {
+
+        document.body.innerHTML = err.message
+    })
+}
